180e66389d89face9c5628386190912b1e6170b0
Bianca Massacci <53439824+BiancaMass@users.noreply.github.com>
2020-05-26 18:23:35 +0200

    Update step_01.R

--- scripts/step_01.R
+++ scripts/step_01.R
@@ -7,9 +7,6 @@ print(args)
 path2_json_file = args[1]
 # **********************************************************************
 
-# Hard coded to test
-# path2_json_file = "~/Documents/senior_project/automated_pca/data/pipeline_input_file.json"
-
 # Load the necessary libraries
 print("*** Loading libraries ***")
 options(stringsAsFactors = FALSE)
